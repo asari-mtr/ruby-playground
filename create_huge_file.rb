091b9b114d760c27b46293997afb203229b042a3
Asari Mitsuteru <asari.mtr@gmail.com>
2018-11-21 20:32:28 +0900

    cooki/idfaを想定したランダムな値を作成

--- create_huge_file.rb
+++ create_huge_file.rb
@@ -1,11 +1,29 @@
 #!/usr/bin/env ruby
 
 require 'securerandom'
+require 'zlib'
+
+if ARGV.size > 1
+  puts "create_huge_file {uuid_count}"
+end
+
+uuid_count = ARGV.size == 0 ? 1_000 : ARGV[0].to_i
 
 # ruby create_huge_file.rb  41.21s user 1.15s system 96% cpu 44.048 total
 File.open("sample.txt","w+") do |f|
-  1_000_000.times do |i|
-    f.write(i)
+  uuid_count.times do |i|
+    case Random.rand(2)
+    when 0 then
+      f.write(SecureRandom.base64(24))
+    else
+      f.write(SecureRandom.uuid)
+    end
     f.write("\n")
   end
 end
+
+# File.open("sample.txt.gz","w") do |f|
+#   zd = Zlib::GzipWriter.new(f)
+#   zd << File.read("sample.txt")
+#   zd.close
+# end
